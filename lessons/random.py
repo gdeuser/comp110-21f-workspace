76cb95c77961f9ded185e7dcf263c5fd022a749d
Grace Deuser <gedeuser@live.unc.edu>
2021-08-29 13:45:39 -0400

    Messign around

---
+++ lessons/random.py
@@ -0,0 +1,6 @@
+"""Just for fun."""
+
+user_name: str = input("Say ya name, idiot. ")
+user_name: str = input("Say it again stupid. ")
+print("Yikes, " + user_name + ", you're pretty dumb, aren't you?")
+print(user_name + ", you ugly. So is your mom.")
