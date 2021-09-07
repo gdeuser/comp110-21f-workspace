3debe175c77550e033d4494620938ca02b5e5948
Grace Deuser <gedeuser@live.unc.edu>
2021-09-07 16:32:37 -0400

    Progress on Exercise 2

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -4,13 +4,13 @@ __author__ = "730306940"
 
 
 beat = input("What beat do you want to repeat? ")
-beat = beat + " "
 repeat: int = int(input("How many times do you want to repeat it? "))
 
 count: int = 0
 while count < repeat:
-    if repeat <= 0:
-        print("No beat...")
+    if repeat > 0:
+        beat_2 = beat + str(" " + beat)
+        count = count + 1
+        print(beat)
     else:
-        print(str(beat * repeat))
-    count = repeat
+        print("No beat...")
