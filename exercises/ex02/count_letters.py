00fded9b8ce8053de727ce2cfe8bc25f1b2f46c4
Grace Deuser <gedeuser@live.unc.edu>
2021-09-07 20:27:12 -0400

    Progress on Exercise 2

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -5,3 +5,12 @@ __author__ = "730306940"
 
 letter = input("What Letter do you want to search for?: ")
 word = input("Enter a word: ")
+i: int = 0
+final: int = 0
+
+while i < len(word):
+    if word[i] == letter:
+        final = final + 1
+    i = i + 1
+
+print(final)
