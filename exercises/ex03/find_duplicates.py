093e0ceb3845a524c0d79daf97def375c27d7424
Grace Deuser <gedeuser@live.unc.edu>
2021-09-14 22:20:08 -0400

    Progress on Exercise 3

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,22 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730306940"
+
+word = input("Enter a word: ")
+length = len(word)
+x: int = 0
+duplicate = False
+
+while x < length:
+    letter = word[x]
+    y: int = 0
+    count: int = 0
+    while y < length:
+        if word[y] == letter:
+            count = count + 1
+        y = y + 1
+    if count >= 2:
+        duplicate = True
+    x = x + 1
+
+print("Found duplicate: " + str(duplicate))
