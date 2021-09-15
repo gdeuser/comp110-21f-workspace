3c72c830d92de4db1667653d2e9ebd94f91628b8
Grace Deuser <gedeuser@live.unc.edu>
2021-09-14 22:47:32 -0400

    Progress on Exercise 3

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -4,11 +4,10 @@ __author__ = "730306940"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
-tree: str = '\U0001F332'
+tree = ""
 depth: int = int(input("Depth: "))
-start: int = 1
 
-while start > 0 and start <= depth:
-    print(tree)
+while depth > 0:
+    depth = depth - 1
     tree = tree + TREE
-    start = start + 1
+    print(tree)
