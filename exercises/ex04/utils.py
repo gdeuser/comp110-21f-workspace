7e9818bbd84e8714addcddb9e544e9bec391d4fe
Grace Deuser <gedeuser@live.unc.edu>
2021-09-28 13:44:50 -0400

    Progress on Exercise 04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,27 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730306940"
 
 
-# TODO: Implement your functions here.
+def all(n: list[int], number: int) -> bool:
+    """Testing if all integers in a list are the same as the input integer."""
+    i: int = 0
+    state: bool = True
+    while i < len(n):
+        if n[i] != number:
+            state = not True
+        i = i + 1
+    return(state)
+
+
+def is_equal(origin: list[int], compare: list[int]) -> bool:
+    """Testing to see if two lists are the exact same."""
+
+
+def max(input: list[int]) -> int:
+    """Finding the largest number in a list of integers."""
+    x: int = 0
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    else:
+        if input[x]
