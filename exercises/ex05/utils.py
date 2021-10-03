38d8c3882eb8685425bae4650503f8cd728b3a88
Grace Deuser <gedeuser@live.unc.edu>
2021-10-03 13:47:35 -0400

    Progress on Exercise 05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,34 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730306940"
 
-# Define your functions below
+
+def only_evens(n: list[int]) -> list[int]:
+    """Finding only the evens in a given list."""
+    start: int = 0
+    even_list: list[int] = []
+    while start < len(n):
+        if n[start] % 2 == 0:
+            even_list.append(int(n[start]))
+        start += 1
+    return(even_list)
+
+
+def sub(origin: list[int], start: int, end: int) -> list[int]:
+    """Creating a sublist from an orginal input list."""
+    sublist: list[int] = []
+    if end > len(origin):
+        end = len(origin)
+    if start < origin[0]:
+        start = 0
+    while start < end:
+        sublist.append(int(origin[start]))
+        start += 1
+    return sublist
+
+
+def concat(first: list[int], second: list[int]) -> list[int]:
+    """Concatenating two lists into a new list."""
+    result: list[int]
+    result = first + second
+    return result
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,54 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+from exercises.ex05.utils import only_evens, sub, concat
+
+__author__ = "730306940"
+
+
+def test_only_evens_empty() -> None:
+    xs: list[int] = []
+    assert only_evens(xs) == []
+
+
+def test_only_evens_no_evens() -> None:
+    xs: list[int] = [1, 3, 5]
+    assert only_evens(xs) == []
+
+
+def test_only_evens_all_evens() -> None:
+    xs: list[int] = [2, 4, 6, 8]
+    assert only_evens(xs) == [2, 4, 6, 8]
+
+
+def test_sub_negative_start() -> None:
+    origin: list[int] = [1, 2, 3, 4, 5]
+    assert sub(origin, -2, 3) == [1, 2, 3]
+
+
+def test_sub_sinlge() -> None:
+    origin: list[int] = [1, 2, 3, 4]
+    assert sub(origin, 0, 1) == [1]
+
+
+def test_sub_whole_list() -> None:
+    origin: list[int] = [1, 2, 3, 4]
+    assert sub(origin, 0, 5) == [1, 2, 3, 4]
+
+
+def test_concat_empty_arguments() -> None:
+    first: list[int] = []
+    second: list[int] = []
+    assert concat(first, second) == []
+
+
+def test_concat_empty_second() -> None:
+    first: list[int] = [1, 2, 3]
+    second: list[int] = []
+    assert concat(first, second) == [1, 2, 3]
+
+
+def test_concat_full_lists() -> None:
+    first: list[int] = [1, 3, 5]
+    second: list[int] = [2, 4, 6]
+    assert concat(first, second) == [1, 3, 5, 2, 4, 6]
