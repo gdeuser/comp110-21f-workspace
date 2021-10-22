f30a94468a5ad6265eaad1e9357989566efab8c5
Grace Deuser <gedeuser@live.unc.edu>
2021-10-21 23:03:42 -0400

    Progress on Exercise 06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,41 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730306940"
 
-# Define your functions below
+
+def invert(og: dict[str, str]) -> dict[str, str]:
+    """Inverting the keys and values of a dictionary."""
+    result: dict[str, str] = dict()
+    for key in og:
+        if og[key] in result:
+            raise KeyError("Cannot have a repeating key value.")
+        result[og[key]] = key
+    return result
+
+
+def favorite_color(color: dict[str, str]) -> str:
+    """Finding the most favorite color in a dictionary."""
+    counting: dict[str, int] = dict()
+    for key in color:
+        if color[key] in counting:
+            counting[color[key]] += 1
+        else:
+            counting[color[key]] = 1
+    current_max: int = 0
+    current_max_color: str = ""
+    for key in counting:
+        if counting[key] > current_max:
+            current_max = counting[key]
+            current_max_color = key
+    return current_max_color
+
+
+def count(start: list[str]) -> dict[str, int]:
+    """Putting items in a list into a dictionary."""
+    counter: dict[str, int] = dict()
+    for item in start:
+        if item in counter:
+            counter[item] += 1
+        else:
+            counter[item] = 1
+    return counter
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,60 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+from exercises.ex06.dictionaries import invert, favorite_color, count
+
+__author__ = "730306940"
+
+
+def test_invert_empty() -> None:
+    """Testing for empty dictionary."""
+    og: dict[str, str] = dict()
+    assert invert(og) == dict()
+
+
+def test_invert_three_key() -> None:
+    """Testing with three keys."""
+    og: dict[str, str] = {"grace": "elizabeth", "mark": "deuser", "kathy": "reilly"}
+    assert invert(og) == {"elizabeth": "grace", "deuser": "mark", "reilly": "kathy"}
+
+
+def test_invert_two_key() -> None:
+    """Testing with two keys."""
+    og: dict[str, str] = {"grace": "deuser", "kathy": "reilly"}
+    assert invert(og) == {"deuser": "grace", "reilly": "kathy"}
+
+
+def test_favorite_color_empty() -> None:
+    """Testing with an empty dictionary."""
+    color: dict[str, str] = dict()
+    assert favorite_color(color) == ""
+
+
+def test_favorite_color_tie() -> None:
+    """Testing for a tie with the colors."""
+    color: dict[str, str] = {"grace": "green", "blake": "yellow", "mark": "red"}
+    assert favorite_color(color) == "green"
+
+
+def test_favorite_color_winner() -> None:
+    """Testing with a winning color."""
+    color: dict[str, str] = {"grace": "green", "blake": "blue", "mark": "red", "kathy": "blue"}
+    assert favorite_color(color) == "blue"
+
+
+def test_count_empty() -> None:
+    """Testing for this program with an empty list."""
+    start: list[str] = []
+    assert count(start) == dict()
+
+
+def test_count_not_repeating() -> None:
+    """Testing for nonrepeating list items."""
+    start: list[str] = ["red", "orange", "yellow"]
+    assert count(start) == {"red": 1, "orange": 1, "yellow": 1}
+
+
+def test_count_repeating() -> None:
+    """Testing for a list with repeated items."""
+    start: list[str] = ["red", "orange", "red", "yellow", "red"]
+    assert count(start) == {"red": 3, "orange": 1, "yellow": 1}
